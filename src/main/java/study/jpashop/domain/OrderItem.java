1ac928a07a05b0fd719630de77efc32f8e2d1040
who-hoo <who.ho3ov@gmail.com>
2022-04-02 04:50:52 +0900

    :sparkles: 엔티티 클래스 개발
    - 회원 : Member
    - 주문 : Order
    - 주문상태 : OrderStatus(enum)
    - 주문상품 : OrderItem
    - 상품 : Item(도서 - Book, 음반 - Album, 영화 - Movie)
    - 배송 : Delivery
    - 배송상태 : DeliveryStatus(enum)
    - 카테고리 : Category
    - 주소 : Address

---
+++ src/main/java/study/jpashop/domain/OrderItem.java
@@ -0,0 +1,36 @@
+package study.jpashop.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import study.jpashop.domain.item.Item;
+
+@Entity
+@Table(name = "order_item")
+@Getter
+@Setter
+public class OrderItem {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private int orderPrice;
+    private int count;
+}
