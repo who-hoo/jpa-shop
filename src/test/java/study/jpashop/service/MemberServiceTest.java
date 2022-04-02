9e203cdcf492a2fba57f1013708beb8bc7f3e0e5
who-hoo <who.ho3ov@gmail.com>
2022-04-02 17:43:16 +0900

    :white_check_mark: 회원 서비스 테스트 코드 작성

---
+++ src/test/java/study/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,51 @@
+package study.jpashop.service;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.jpashop.domain.Member;
+import study.jpashop.repository.MemberRepository;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("kim");
+
+        //when
+        Long savedId = memberService.join(member);
+
+        //then
+        assertThat(memberRepository.findOne(savedId)).isEqualTo(member);
+    }
+
+    @Test()
+    void 중복_회원_예외() throws Exception {
+        //given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        //when
+        memberService.join(member1);
+
+        //then
+        assertThatThrownBy(() -> memberService.join(member2))
+            .isInstanceOf(IllegalStateException.class)
+            .hasMessage("이미 존재하는 회원입니다.");
+    }
+}
