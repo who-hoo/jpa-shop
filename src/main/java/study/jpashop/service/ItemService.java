97b19f410c72726f94732421fe37eb911595f779
who-hoo <who.ho3ov@gmail.com>
2022-04-30 02:24:51 +0900

    :sparkles: 상품 서비스 개발

---
+++ src/main/java/study/jpashop/service/ItemService.java
@@ -0,0 +1,29 @@
+package study.jpashop.service;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import study.jpashop.domain.item.Item;
+import study.jpashop.repository.ItemRepository;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+}
