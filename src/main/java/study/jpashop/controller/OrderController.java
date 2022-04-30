bfaf2e415eb9185a8d7fa64d3c9da2ccbca3a034
who-hoo <who.ho3ov@gmail.com>
2022-05-01 04:58:49 +0900

    :sparkles: 상품 주문 기능 구현

---
+++ src/main/java/study/jpashop/controller/OrderController.java
@@ -0,0 +1,42 @@
+package study.jpashop.controller;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import study.jpashop.domain.Member;
+import study.jpashop.domain.item.Item;
+import study.jpashop.service.ItemService;
+import study.jpashop.service.MemberService;
+import study.jpashop.service.OrderService;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping("/order")
+    public String createForm(Model model) {
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count) {
+        orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+}
